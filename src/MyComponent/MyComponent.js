a93c2a697be745f3ac9477b887bd4ac786606ead
Dinesh <flexdinesh@gmail.com>
2018-01-28 00:27:53 +0530

    Boilerplate initial port and setup

---
+++ src/MyComponent/MyComponent.js
@@ -0,0 +1,28 @@
+// Demo component
+// this is only example component
+
+import React from 'react';
+
+// import './MyComponent.style';
+
+class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div className='my-component'>
+                <i className='icon-test'>One</i><br></br>
+                <i className='icon-test'>Two</i><br></br>
+                <i className='icon-test'>Three</i>
+            </div>
+        )
+    }
+}
+
+export default MyComponent;
